61a59ca0698bfdc4d34ac86ccfe55df01ba642b1
Bill Chatfield <bill_chatfield@yahoo.com>
2019-10-09 22:13:35 -0400

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
-javac -d classes -sourcepath src -target 1.4 || exit /b %ERRORLEVEL%
-jar -cvf hreodwrit.jar -C classes * || exit /b %ERRORLEVEL%
-jar -uvf hreodwrit.jar -C resources * || exit /b %ERRORLEVEL%
+javac -d classes -sourcepath src -source 1.4 -target 1.4 src/com/alteredmechanism/notepad/*.java src/com/alteredmechanism/java/awt/*.java src/com/alteredmechanism/javax/swing/*.java || exit /b %ERRORLEVEL%
+jar -cvf hreodwrit.jar -C classes . || exit /b %ERRORLEVEL%
+jar -uvf hreodwrit.jar -C resources . || exit /b %ERRORLEVEL%
+rem Still need to create correct manifest
 
