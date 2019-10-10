b71f46c82e9bb366b24469c1cd272cc2a69923e0
Bill Chatfield <bill.chatfield@cardinalhealth.com>
2019-10-10 15:02:11 -0400

    Improve startup

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
 @echo off
-javac -d classes -sourcepath src -source 1.4 -target 1.4 src/com/alteredmechanism/notepad/*.java src/com/alteredmechanism/java/awt/*.java src/com/alteredmechanism/javax/swing/*.java || exit /b %ERRORLEVEL%
-jar -cvf hreodwrit.jar -C classes . || exit /b %ERRORLEVEL%
-jar -uvf hreodwrit.jar -C resources . || exit /b %ERRORLEVEL%
-rem Still need to create correct manifest
+setlocal
+set JAVA_VERSION=1.4
+set APPLICATION_JAR=hreodwrit.jar
+javac -d classes -sourcepath src ^
+    -source %JAVA_VERSION% -target %JAVA_VERSION% ^
+    src\com\alteredmechanism\notepad\*.java ^
+    src\com\alteredmechanism\java\awt\*.java ^
+    src\com\alteredmechanism\javax\swing\*.java || exit /b %ERRORLEVEL%
+jar -cvf lib\%APPLICATION_JAR% -C classes . || exit /b %ERRORLEVEL%
+jar -uvf lib\%APPLICATION_JAR% -C resources . || exit /b %ERRORLEVEL%
+rem Still need to create correct manifest for runnable jar
+endlocal
 
