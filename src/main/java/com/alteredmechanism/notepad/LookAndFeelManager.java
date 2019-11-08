594bc1244c6e3e9ce7230827a2634bfedac9ba4c
Bill Chatfield <bill_chatfield@yahoo.com>
2019-11-08 01:33:44 -0500

    Try to change color

--- src/main/java/com/alteredmechanism/notepad/LookAndFeelManager.java
+++ src/main/java/com/alteredmechanism/notepad/LookAndFeelManager.java
@@ -1,4 +1,5 @@
 package com.alteredmechanism.notepad;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,20 +7,34 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.plaf.ColorUIResource;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.MetalTheme;
 
 
 public class LookAndFeelManager implements ActionListener {
-    
+	
+	public static final String OCEAN_THEME_CLASS_NAME = "javax.swing.plaf.metal.OceanTheme";
+	public static final String DEFAULT_METAL_THEME_CLASS_NAME = "javax.swing.plaf.metal.DefaultMetalTheme";
+	public static final String METAL_STEEL_THEME_ID = "Metal with Steel Theme";
+	public static final String METAL_OCEAN_THEME_ID = "Metal";
+	public static final String MOTIF_THEME_ID = "CDE/Motif";
+
+
     protected ButtonGroup buttonGroup = new ButtonGroup();
     private Messenger messenger;
     private Map lookMap = new TreeMap();
     private List componentsToUpdate = new ArrayList();
+    protected LookAndFeelInfo metalPlaf = null;
+    protected Class oceanThemeClass = null;
     
     public static void setSystemLookAndFeel() {
         // Default to the operating system's native look and feel. Duh...
@@ -47,6 +62,9 @@ public class LookAndFeelManager implements ActionListener {
         LookAndFeelInfo[] looks = UIManager.getInstalledLookAndFeels();
         for (int i = 0; i < looks.length; i++) {
             LookAndFeelInfo look = looks[i];
+            if (look.getName().equals("Metal")) {
+            	metalPlaf = look;
+            }
             lookMap.put(look.getName(), look);
             JRadioButtonMenuItem lookMenuItem = new JRadioButtonMenuItem(look.getName());
             lookMenuItem.addActionListener(this);
@@ -56,6 +74,18 @@ public class LookAndFeelManager implements ActionListener {
             	lookMenuItem.setSelected(true);
             }
         }
+        try {
+			oceanThemeClass = Class.forName(OCEAN_THEME_CLASS_NAME);
+			Class.forName(DEFAULT_METAL_THEME_CLASS_NAME);
+			String lookName = METAL_STEEL_THEME_ID;
+			lookMap.put(lookName, metalPlaf);
+			JRadioButtonMenuItem lookMenuItem = new JRadioButtonMenuItem(lookName);
+            lookMenuItem.addActionListener(this);
+            buttonGroup.add(lookMenuItem);
+            lookAndFeelMenu.add(lookMenuItem);
+		} catch (ClassNotFoundException e) {
+			System.err.println("No steel theme");
+		}
     }
     
     public void addComponentToUpdate(Component c) {
@@ -74,18 +104,30 @@ public class LookAndFeelManager implements ActionListener {
         catch (ClassNotFoundException e) {
             // If Open Look is not available, just do nothing.
         }
-    }
+    } 
 
     public void actionPerformed(ActionEvent event) {
         System.out.println(event.paramString());
         try {
+        	// The theme must be set before setting the look and feel
+        	//  to Metal.
+        	if (METAL_STEEL_THEME_ID.equals(event.getActionCommand())) {
+        		MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
+        	}
+        	else if (METAL_OCEAN_THEME_ID.equals(event.getActionCommand())) {
+        		MetalTheme oceanThemeInstance = (MetalTheme) oceanThemeClass.newInstance();
+        		MetalLookAndFeel.setCurrentTheme(oceanThemeInstance);        		
+        	}
             UIManager.setLookAndFeel(((LookAndFeelInfo)lookMap.get(event.getActionCommand())).getClassName());
+        	// Must be done after setting LAF.
+            if (MOTIF_THEME_ID.equals(event.getActionCommand())) {
+        		System.out.println("Setting Motif Blue");
+        		Color motifBlue = new ColorUIResource(124, 155, 255);
+        		UIManager.put("window", motifBlue);
+        	}
             for (int i = 0; i < componentsToUpdate.size(); i++) {
                 Component c = (Component) componentsToUpdate.get(i);
                 SwingUtilities.updateComponentTreeUI(c);
-//                if (c instanceof Window) {
-//                    ((Window) c).pack();
-//                }
             }
         }
         catch (Exception e) {
