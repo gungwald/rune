2391e5bb099c84d6fc422423de517284c7010702
Bill Chatfield <bill_chatfield@yahoo.com>
2022-02-13 11:15:28 -0500

    Update status bar

--- src/main/java/com/alteredmechanism/notepad/StatusBar.java
+++ src/main/java/com/alteredmechanism/notepad/StatusBar.java
@@ -1,19 +1,29 @@
 package com.alteredmechanism.notepad;
 
-import javax.swing.*;
+import java.awt.ComponentOrientation;
+import java.awt.Insets;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
-import java.awt.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class StatusBar extends JPanel {
+    private static final long serialVersionUID = 1L;
     protected Rune creator;
     protected JLabel cursorPosition;
 
     public StatusBar(Rune creator) {
         setBorder(new BevelBorder(BevelBorder.LOWERED));
-        setPreferredSize(new Dimension(creator.getWidth(), 16));
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        cursorPosition = new JLabel("1:1");
-        cursorPosition.setHorizontalAlignment(SwingConstants.RIGHT);
+        //setPreferredSize(new Dimension(creator.getWidth(), 16));
+        setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+        cursorPosition = new JLabel("1:1", SwingConstants.RIGHT);
+        Border positionBorder = new EmptyBorder(new Insets(3, 3, 3, 3));
+        cursorPosition.setBorder(positionBorder);
         add(cursorPosition);
     }
 
