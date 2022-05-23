aad5c628f152a114881ff5f82caee2419ce0924a
Bill Chatfield <bill_chatfield@yahoo.com>
2022-05-23 09:18:14 -0400

    Added command line parsing for pid

--- src/main/c/rune-out-of-memory-error-handler.c
+++ src/main/c/rune-out-of-memory-error-handler.c
@@ -3,13 +3,21 @@
 
 int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prevInstance, LPWSTR commandLine, int commandShow)
 {
+	/* Get command line, convert to argv, get pid argument, kill process */
+	int argCount;
+	wchar_t **args;
+       
+	args = CommandLineToArgvW(GetCommandLineW(), &argCount);
+
+	if (argCount > 1) {
+		wchar_t *pid = args[1];
+	}
+
 	MessageBoxW(
 		NULL, 
 		L"An OutOfMemoryError has occurred with Rune. It will now be closed.", 
 		NULL,
 		MB_OK);
-
-	/* Get command line, convert to argv, get pid argument, kill process */
 
 	return EXIT_SUCCESS;
 }
