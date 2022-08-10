354c3625e4d9532b4ae68f3fcd2b8880f8a1057c
Bill Chatfield <bill_chatfield@yahoo.com>
2022-08-09 23:12:39 -0400

    Rename common-functions.sh

--- src/main/scripts/rune-out-of-mem-err-handler.sh
+++ src/main/scripts/rune-out-of-mem-err-handler.sh
@@ -4,5 +4,5 @@
 
 # shellcheck disable=SC2006
 SCRIPT_DIR=`dirname "$0"` && [ "$SCRIPT_DIR" = "." ] && SCRIPT_DIR=`pwd`
-. "$SCRIPT_DIR"/common-functions.sh
+. "$SCRIPT_DIR"/rune-common-functions.sh
 displayError Rune ran out of memory.
